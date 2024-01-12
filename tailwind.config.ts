7c883ff20dceb1dda19917e8462f6b9adb6b60cc
higorsantana-omega <higor.santanahs17@gmail.com>
2024-01-11 21:54:32 -0300

    chore(tailwind): set primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        primary: '#0078d4'
+      }
     },
   },
   plugins: [],
