ca2e89dd7d0786e712256b4781ec756d6a4365c2
higorsantana-omega <higor.santanahs17@gmail.com>
2024-01-12 23:02:07 -0300

    feat(typing): import character component

--- src/layout/Typing.tsx
+++ src/layout/Typing.tsx
@@ -1,18 +1,26 @@
+import { Character } from "./Character"
 import { Cursor } from "./Cursor"
 
 export interface TypingProps {
   typedCharacters: string
+  words: string
   className?: string
 }
 
-export function Typing ({ typedCharacters, className }: TypingProps) {
+export function Typing ({ typedCharacters, words, className }: TypingProps) {
   const characters = typedCharacters.split('')
 
   return (
     <div className={`${className}`}>
       {
         characters.map((character, index) => {
-          return <span key={`${character}_${index}`} className='text-primary'>{character}</span>
+          return (
+            <Character
+              key={`${character}_${index}`}
+              character={character} 
+              expected={words[index]!}
+            />
+          )
         })
       }
 
